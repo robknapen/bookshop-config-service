bf0bdeec38bce66c52d3392f50bf91b282ad850b
Rob Knapen <rob.knapen@wur.nl>
2023-03-19 14:17:02 +0100

    Initial commit

---
+++ src/main/java/com/polarbookshop/configservice/BookshopConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopConfigServiceApplication.class, args);
+    }
+
+}
